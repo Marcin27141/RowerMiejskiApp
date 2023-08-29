0adf0ef18d2a499ddb9bc431c7b9731446292171
Marcin <marcin.tkocz@op.pl>
2023-08-29 08:53:59 +0200

    admin/user radio group

--- app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup roleRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        roleRadioGroup = findViewById(R.id.roleRadioGrp);
+        roleRadioGroup.setOnCheckedChangeListener((radioGroup, i) -> {
+            if (i == R.id.isAdminRadioBtn) {
+                Toast.makeText(MainActivity.this, "Admin", Toast.LENGTH_SHORT).show();
+            } else if (i == R.id.isUserRadioBtn) {
+                Toast.makeText(MainActivity.this, "User", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void onBtnClick(View view) {
@@ -29,5 +39,7 @@ public class MainActivity extends AppCompatActivity {
         firstNameResult.setText(firstNameTxt.getText());
         lastNameResult.setText(lastNameTxt.getText());
         emailResult.setText(emailTxt.getText());
+
+        Toast.makeText(this, String.format("%s %s, %s", firstNameTxt.getText(), lastNameTxt.getText(), emailTxt.getText()), Toast.LENGTH_SHORT).show();
     }
 }
