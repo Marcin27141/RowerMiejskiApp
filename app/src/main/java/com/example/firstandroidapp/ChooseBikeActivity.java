5fe5f2c624f8da844b7379f295d5c883775c23e6
Marcin <marcin.tkocz@op.pl>
2023-09-02 11:37:09 +0200

    rounded popup dialog, color changes

--- app/src/main/java/com/example/firstandroidapp/ChooseBikeActivity.java
+++ app/src/main/java/com/example/firstandroidapp/ChooseBikeActivity.java
@@ -3,6 +3,8 @@ package com.example.firstandroidapp;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
